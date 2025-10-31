1553ab2556aabb327f710daea7b2b0239a14489a
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-10-31 16:40:20 +0100

    Update console output message for report count and adjust assembly informational version

--- Program.cs
+++ Program.cs
@@ -13,13 +13,20 @@ var reports = reader.Read(path);
 // I'm reading the lines.
 
 Console.WriteLine(reports.Count);
-Console.WriteLine($"вот столько строк {reports.Count}");
+Console.WriteLine($"number of terms in the database {reports.Count}");
 
 var controller = new ReportController(reports);
 Console.WriteLine("\nThe first five products ");
 
 foreach (var name in controller.SelectAllProdictNames().Take(5))
     Console.WriteLine($"{name}");
+
+
+
+Console.WriteLine("\nThe first five IndustryName");
+//IndustryName
+foreach (var industryName in controller.SelectAllProdictNames().Take(5))
+    Console.WriteLine($"{industryName}");
 
 //I take the first five products and put them in the console.
 
